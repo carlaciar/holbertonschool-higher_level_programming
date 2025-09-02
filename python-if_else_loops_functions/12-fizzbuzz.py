74df40f64cf1e70ca49909115cf4b94345cd3bb6
carlaciar <carlaciarr@gmail.com>
2025-09-03 08:51:35 +1000

    add 12-fizzbuzz.py

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    i = 1
+
+    while i < 100:
+        if i % 3 == 0:
+            print("Fizz", end=" ")
+        
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        
+        elif i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        
+        else:
+            print(f"{i}", end=" ")
+        
+        i += 1
