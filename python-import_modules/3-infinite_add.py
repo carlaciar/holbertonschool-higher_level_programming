5ffb77b2c18c83800cd2aabaa5818f9ab5f85d75
carlaciar <carlaciarr@gmail.com>
2025-09-03 13:20:57 +1000

    add 3-infinite_add.py

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+total = 0
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("0")
+    
+    if len(sys.argv) > 1:
+        for arg in sys.argv[1:]:
+            total += int(arg)
+        print(total)
