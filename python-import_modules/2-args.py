8f0b4e5b1f6b61314333408b832a7434bebce182
carlaciar <carlaciarr@gmail.com>
2025-09-03 12:59:00 +1000

    add 2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+
+    if len(sys.argv) == 2:
+        print("1 argument.")
+
+        for i in range(len(sys.argv) - 1):
+            print("{} : {}".format(i + 1, sys.argv[i + 1]))
+
+    if len(sys.argv) > 2:
+        print(len(sys.argv) - 1, "arguments.")
+
+        for i in range(len(sys.argv) - 1):
+            print("{} : {}".format(i + 1, sys.argv[i + 1]))
