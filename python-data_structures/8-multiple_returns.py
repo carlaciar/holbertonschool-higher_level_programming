413abaa77ce7270f13e0a2e5c925a3fa4242c0d7
carlaciar <carlaciarr@gmail.com>
2025-09-09 14:31:58 +1000

    edit 8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    first = sentence[0]
+    length = 0
 
-    if sentence == None:
-        first = None
+    if sentence != "":
+        length = len(sentence)
+        first = sentence[0]
+
+    if sentence == "":
+        first = "None"
 
     multiplereturnstuple = ([length, first])
     return multiplereturnstuple
