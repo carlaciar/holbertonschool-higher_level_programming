a1f0c8365d6a5c02fe08c86bbe0ee3ac6d06bfc9
carlaciar <carlaciarr@gmail.com>
2025-09-09 14:16:09 +1000

    fix pycodestyle

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -11,15 +11,13 @@ def add_tuple(tuple_a=(), tuple_b=()):
         list_a = list(tuple_a)
         list_a.append(0)
         list_a.append(0)
-    
+
     if len(tuple_b) < 2:
         list_b = list(tuple_b)
         list_b.append(0)
         list_b.append(0)
-    
+
     x = list_a[0] + list_b[0]
     y = list_a[1] + list_b[1]
     addtuple = tuple([x, y])
     return addtuple
-
-    
