1d1e7ebfe08f5c1bab0d84e753566c9cacca4992
carlaciar <carlaciarr@gmail.com>
2025-09-09 17:34:30 +1000

    fix pycodestyle

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -5,7 +5,7 @@ def square_matrix_simple(matrix=[]):
     for row in matrix:               # loop through each row
         new_row = []                 # new row to store squared values
         for num in row:              # loop through each number in the row
-            new_row.append(num ** 2) # square the number
+            new_row.append(num ** 2)  # square the number
         copy.append(new_row)         # add the new row to the new matrix
 
     return copy
