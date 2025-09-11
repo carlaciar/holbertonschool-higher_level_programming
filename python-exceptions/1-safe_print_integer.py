1545bbe132d4b498e739593fa75473a658e5552a
carlaciar <carlaciarr@gmail.com>
2025-09-11 17:22:10 +1000

    add 1-safe_print_integer.py

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        if value % 1 == 0:
+            print("{:d}".format(value))
+            return True
+        
+        else:
+            return False
+    
+    except TypeError:
+        return False
