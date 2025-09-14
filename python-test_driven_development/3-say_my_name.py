3a0028c418bd433e8bed0792b80fff4333a6ac48
carlaciar <carlaciarr@gmail.com>
2025-09-14 23:13:47 +1000

    edit 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,24 +4,35 @@ Print a full name as: 'My name is <first name> <last name>'.
 """
 
 
-def say_my_name(first_name, last_name=""):
+def say_my_name(*args):
     """
-    Print "My name is <first name> <last name>".
-
-    Args:
-        first_name (str): The first name.
-        last_name (str): The last name (optional).
-
-    Raises:
-        TypeError: If first_name or last_name is not a string.
+    Accepts 1 or 2 positional args: first_name [, last_name].
+    Prints "My name is <first_name> <last_name>".
     """
+    argc = len(args)
+    if argc == 0:
+        # Match CPython's 2-missing-args wording
+        raise TypeError(
+            "say_my_name() missing 2 required positional arguments: "
+            "'first_name' and 'last_name'"
+        )
+    if argc == 1:
+        first_name = args[0]
+        last_name = ""
+    elif argc == 2:
+        first_name, last_name = args
+    else:
+        raise TypeError(
+            "say_my_name() takes from 1 to 2 positional arguments but {} were given"
+            .format(argc)
+        )
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
     if last_name == "":
-        # Checker expects a trailing space when last_name is empty
-        print("My name is {} ".format(first_name))
+        print("My name is {} ".format(first_name))  # trailing space required
     else:
         print("My name is {} {}".format(first_name, last_name))
