55d4c49b52a13e71697987ebbd6fb4b16756fb74
carlaciar <carlaciarr@gmail.com>
2025-09-16 15:50:17 +1000

    add 2-rectangle.py

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+This module defines the Rectangle class.
+
+The Rectangle class provides:
+- A private instance attribute: width and height
+- A property to retrieve width and height
+- A setter to set width and height with type and value validation
+- Instantiation with optional width and height (default is 0)
+"""
+
+
+class Rectangle:
+    """Defines a square with a private width and height"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int):
+            self.__width = value
+
+        else:
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int):
+            self.__height = value
+
+        else:
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+    def area(self):
+        return int(self.__width) * int(self.__height)
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+
+        else:
+            return 2 * (int(self.__width) + int(self.__height))
