f577cc451ef6630d74ebf999a673037d9e6012b4
carlaciar <carlaciarr@gmail.com>
2025-10-01 11:34:52 +1000

    add 0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module that defines a function to read and print a text file (UTF8).
+"""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints its contents to stdout."""
+
+    with open("my_file_0.txt", encoding="utf-8") as f:
+        print(f.read())
---
+++ python-input_output/README.md
@@ -0,0 +1,1 @@
+# Sprint 2, Week 5: Python - Input/Output
