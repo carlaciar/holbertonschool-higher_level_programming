2c121c0490bd7944737800a79fb7a305657e4bea
carlaciar <carlaciarr@gmail.com>
2025-10-01 11:35:47 +1000

    edit 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
     """Reads a text file (UTF8) and prints its contents to stdout."""
 
     with open("my_file_0.txt", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
