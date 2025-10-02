8383246c1526d2300800c0527c348a2e088bc2c2
carlaciar <carlaciarr@gmail.com>
2025-10-02 10:47:40 +1000

    add task_01_pickle.py

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env
+"""
+this module serializes and deserializes custom
+Python objects using the pickle module
+"""
+
+import pickle
+
+
+class CustomObject:
+    """
+    serializes and deserializes custom
+    Python objects using the pickle module
+    """
+
+    def __init__(self, name="", age=0, is_student=False):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print("Name: {}".format(self.name))
+        print("Age: {}".format(self.age))
+        print("Is Student: {}".format(self.is_student))
+
+    def serialize(self, filename):
+        with open(filename, "wb") as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, "rb") as f:
+            loaded = pickle.load(f)
+            return loaded
