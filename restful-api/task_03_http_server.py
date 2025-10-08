40ed573d30c0a292d5eeafbdb043ae74ce68c874
carlaciar <carlaciarr@gmail.com>
2025-10-08 15:58:10 +1100

    edit

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -41,7 +41,6 @@ class Handler(BaseHTTPRequestHandler):
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain; charset=utf-8")
-            self.send_header("Content-Length", "0")
             self.end_headers()
             self.wfile.write(b"404 not found")
 
