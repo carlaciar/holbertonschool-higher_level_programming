0670a4e4437fae8db3f2944c81f5538de016c389
carlaciar <carlaciarr@gmail.com>
2025-10-17 15:20:55 +1100

    edit task6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,6 +2,8 @@
 -- and the table states (in the database hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS states (
     id INT UNIQUE NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
