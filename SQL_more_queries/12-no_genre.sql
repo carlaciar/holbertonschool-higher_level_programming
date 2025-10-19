15537f8ecf2749e1b70152e9bc5ea9c11a89b6d8
carlaciar <carlaciarr@gmail.com>
2025-10-19 19:57:00 +1100

    add 12-no_genre.sql

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,9 @@
+-- script that lists all shows without a genre linked
+-- Each record: tv_shows.title - tv_show_genres.genre_id
+-- Results sorted by tv_shows.title and tv_show_genres.genre_id
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
