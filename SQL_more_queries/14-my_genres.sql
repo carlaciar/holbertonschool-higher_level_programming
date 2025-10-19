24d72c3c6f4768f9df00ad70be8e45e8cf986dc4
carlaciar <carlaciarr@gmail.com>
2025-10-19 20:00:31 +1100

    add 14-my_genres.sql

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+-- script that lists all genres of the show Dexter
+-- Each record: tv_genres.name
+-- Results sorted by genre name ascending
+
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
