3bc17afce746d30449a2d5ed9972c57fd356696f
carlaciar <carlaciarr@gmail.com>
2025-11-02 22:30:31 +1100

    add 7

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const value = args[0];
+const count = parseInt(args[0]);
+
+if (isNaN(value)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < count; i++) {
+    console.log('C is fun');
+  }
+}
