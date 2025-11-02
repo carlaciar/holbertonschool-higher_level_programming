54a7dd1b21786ab073d32274d6d0cd4a2e8d5ff3
carlaciar <carlaciarr@gmail.com>
2025-11-02 21:55:32 +1100

    add 5

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const value = args[0];
+
+if (isNaN(value)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ', parseInt(value));
+}
